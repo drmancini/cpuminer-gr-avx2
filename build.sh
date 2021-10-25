a6e248398485a93f44cb473fd43764a5e754cc44
Michal Zurkowski <michal.zurkowski@gmail.com>
2021-10-25 22:35:00 +0200

    Update build.sh to allow uArch change.

--- build.sh
+++ build.sh
@@ -8,17 +8,23 @@ rm -f config.status
 GCC_VERSION=$(gcc --version | grep ^gcc | sed 's/^.* //g')
 GCC_MAJOR=$(echo $GCC_VERSION | cut -d. -f1)
 
+# Allow for different uArch via build argument.
+MARCH="native"
+if [[ ! -z ${1} ]]; then
+  MARCH="${1}"
+fi
+
 echo "Detected GCC ${GCC_VERSION} with Major ${GCC_MAJOR}"
 
 if [[ $GCC_MAJOR == 8 || $GCC_MAJOR == 9 ]]; then
 
-  CFLAGS="-O3 -march=native -mtune=native" \
+  CFLAGS="-O3 -march=${MARCH} -mtune=${MARCH}" \
   CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
   ./configure
 
 elif [[ $GCC_MAJOR -ge 10 ]]; then
 
-  CFLAGS="-O3 -march=native -mtune=native" \
+  CFLAGS="-O3 -march=${MARCH} -mtune=${MARCH}" \
   CXXFLAGS="$CFLAGS -std=c++20 -Wno-ignored-attributes" \
   ./configure
 
